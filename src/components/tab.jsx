a873271eb08204e7e2070700892dcf4349e81b70
shivam2kraj <shivam2kraj@gmail.com>
2024-10-24 22:40:53 +0530

    commit

--- src/components/tab.jsx
+++ src/components/tab.jsx
@@ -1,46 +1,4 @@
 
-// import React, { useState } from 'react';
-// import OneTrip from './Forms/oneTrip'
-// import RoundTrip from './Forms/roundTrip';
-// import MultiLeg from './Forms/multiLeg';
-
-// const TabComponent = () => {
-//   const [activeTab, setActiveTab] = useState('tab1');
-
-//   const handleTabClick = (tab) => {
-//     setActiveTab(tab);
-//   };
-
-//   return (
-//     <div className="justify items-center">
-//       {/* Tab navigation */}
-//       <div className="flex justify-center gap-4 md:gap-[30px] mb-8">
-//         {['One Trip', 'Round Trip', 'Multi Leg'].map((tab, index) => (
-//           <button
-//             key={index}
-//             className={`hover:shadow-[0px_0px_20px_grey] transition-all duration-500 hover:border-white w-[100px] rounded-[5px] py-2 ${
-//               activeTab === `tab${index + 1}` ? 'bg-white text-black' : 'bg-black text-white'
-//             }`}
-//             onClick={() => handleTabClick(`tab${index + 1}`)}
-//           >
-//             {tab}
-//           </button>
-//         ))}
-//       </div>
-
-//       {/* Content for each tab */}
-//       <div>
-//         {activeTab === 'tab1' && <OneTrip />}
-//         {activeTab === 'tab2' && <RoundTrip />}
-//         {activeTab === 'tab3' && <MultiLeg />}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TabComponent;
-
-
 import React, { useState } from 'react';
 import OneTrip from './Forms/oneTrip';
 import RoundTrip from './Forms/roundTrip';
